a46542ff33c3002847f4157fb583b6631e7d657c
Alex Kirk <akirk@users.noreply.github.com>
2025-08-28 15:08:41 +0200

    Add support for Linear links

--- index.php
+++ index.php
@@ -363,6 +363,10 @@ function load_team_config( $team_slug = 'team' ) {
 				$links['HR monthly'] = $member_data['hr_feedback'];
 			}
 		}
+
+		if ( isset( $member_data['linear'] ) ) {
+			$links['Linear'] = 'https://linear.app/a8c/profiles/' . $member_data['linear'];
+		}
 		
 		$person = new Person( 
 			$member_data['name'], 
@@ -1107,17 +1111,27 @@ $available_teams = get_available_teams();
                         <a href="<?php echo build_team_url( 'admin.php', array( 'edit_member' => $person ) ); ?>" target="_blank">✏️ Edit Person</a>
                         <?php foreach ( $person_data->links as $link_text => $link_url ) : ?>
                             <?php if ( ! empty( $link_url ) ) : ?>
-                                <a href="<?php echo htmlspecialchars( $link_url ); ?>" target="_blank">📄 <?php echo htmlspecialchars( $link_text ); ?></a>
+                                <a href="<?php echo htmlspecialchars( $link_url ); ?>" target="_blank"><?php
+                                if ( 0 === strpos( $link_url, 'https://linear.app/' ) ) {
+                                	?><svg xmlns="http://www.w3.org/2000/svg" fill="none" width="16" height="16" viewBox="0 0 100 100" style="vertical-align: middle"><path fill="#222326" d="M1.22541 61.5228c-.2225-.9485.90748-1.5459 1.59638-.857L39.3342 97.1782c.6889.6889.0915 1.8189-.857 1.5964C20.0515 94.4522 5.54779 79.9485 1.22541 61.5228ZM.00189135 46.8891c-.01764375.2833.08887215.5599.28957165.7606L52.3503 99.7085c.2007.2007.4773.3075.7606.2896 2.3692-.1476 4.6938-.46 6.9624-.9259.7645-.157 1.0301-1.0963.4782-1.6481L2.57595 39.4485c-.55186-.5519-1.49117-.2863-1.648174.4782-.465915 2.2686-.77832 4.5932-.92588465 6.9624ZM4.21093 29.7054c-.16649.3738-.08169.8106.20765 1.1l64.77602 64.776c.2894.2894.7262.3742 1.1.2077 1.7861-.7956 3.5171-1.6927 5.1855-2.684.5521-.328.6373-1.0867.1832-1.5407L8.43566 24.3367c-.45409-.4541-1.21271-.3689-1.54074.1832-.99132 1.6684-1.88843 3.3994-2.68399 5.1855ZM12.6587 18.074c-.3701-.3701-.393-.9637-.0443-1.3541C21.7795 6.45931 35.1114 0 49.9519 0 77.5927 0 100 22.4073 100 50.0481c0 14.8405-6.4593 28.1724-16.7199 37.3375-.3903.3487-.984.3258-1.3542-.0443L12.6587 18.074Z"/></svg> <?php
+                                } else {
+                                	?>📄<?php
+                                }
+                                echo htmlspecialchars( $link_text ); ?></a>
                             <?php endif; ?>
                         <?php endforeach; ?>
                         <?php if ( $is_team_member && ! empty( $person_data->username ) ) : ?>
                             <?php
                             $last_month = date( 'Y-m', strtotime( 'last month') );
                             $start_date = $last_month . '-01';
-                            $end_date = date( 'Y-m-t', strtotime( $start_date ) );
+                            $end_date = date( 'Y-m-d', strtotime( $start_date ) );
                             $activity_url = $team_data['activity_url_prefix'] . '&member=' . urlencode( $person_data->username ) . "&start={$start_date}&end={$end_date}";
                             ?>
-                            <a href="<?php echo htmlspecialchars( $activity_url ); ?>" target="_blank" class="activity-link">📊 Team Activity (Last Month)</a>
+                            📊 Team Activity <a href="<?php echo htmlspecialchars( $activity_url ); ?>" target="_blank" class="activity-link">Last Month</a>
+                            <?php
+                            $activity_url = $team_data['activity_url_prefix'] . '&member=' . urlencode( $person_data->username );
+                            ?>
+                            <a href="<?php echo htmlspecialchars( $activity_url ); ?>" target="_blank" class="activity-link">Last Week</a>
                         <?php elseif ( $is_team_member ) : ?>
                             <span style="color: #666; font-style: italic;">Team Activity (Username not configured)</span>
                         <?php endif; ?>
